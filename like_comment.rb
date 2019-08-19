5834d1193d3b0e65bb542bd6cd1631840c24eee1
reiya <reiya.ono@linkbal.co.jp>
2019-08-19 19:04:52 +0900

    refactoring

--- like_comment.rb
+++ like_comment.rb
@@ -7,23 +7,29 @@ d = Selenium::WebDriver.for :chrome
 gmail = ARGV[0]
 password = ARGV[1]
 
+def fill_input(d, element_name, element_value, input_value)
+  d&.find_element(element_name, element_value)&.send_keys(input_value)
+  d&.find_element(element_name, element_value).send_keys(:enter)
+  sleep 2 
+end
+
 d.get "https://accounts.google.com/ServiceLogin/identifier"
 sleep 2
-d&.find_element(:id, 'identifierId')&.send_keys(gmail)
-d&.find_element(:id, 'identifierId').send_keys(:enter)
-sleep 1
-d&.find_element(:class, 'zHQkBf')&.send_keys(password)
-d&.find_element(:class, 'zHQkBf')&.send_keys(:enter)
-sleep 2
+fill_input(d, :id, 'identifierId', gmail)
+fill_input(d, :class, 'zHQkBf', password)
 d.get "https://www.youtube.com/comments"
 sleep 2
 d.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-sleep 4
+sleep 2
+d&.find_element(:id, 'yt-comments-paginator')&.click
+sleep 5
 
 begin
  hearts = d&.find_elements(:class, 'creator-heart-big-unhearted')
  hearts.each do |heart|
   p heart
+  heart.click
+  sleep 1
  end
  p "Liked heart: #{hearts.count}"
 rescue Selenium::WebDriver::Error::NoSuchElementError
